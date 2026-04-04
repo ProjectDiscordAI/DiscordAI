9a8cdd7d1b997853574151d5152a88d9b413babd
只是蘋果JustApple <114324056+JustappleJust@users.noreply.github.com>
2026-04-04 10:44:19 +0800

    banner update

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
 /*
 DiscordAI v2
+The best AI bot framework on Discord.
 
 by JustApple
 */
+
+// dependencies
+const fs = require('fs');
+
+// startup log (just for fun, nothing special)
+require('./utils/startup-logger.js');
+
+// 
